4db5f1166172d93f77edf7053e6b29a6e56de8a5
JoseVinicius-P <josevpf123@gmail.com>
2023-06-22 17:58:59 -0300

    feat: Adding responsive builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() => runApp(ModularApp(module: AppModule(), child: AppWidget()));
+void main() => runApp(
+    ResponsiveApp(builder: (context) => ModularApp(module: AppModule(), child: AppWidget())),
+);
