cc718c567389a5ff77ce08c1c04a81e0c91f32f3
JoseVinicius-P <josevpf123@gmail.com>
2023-07-02 11:58:36 -0300

    feat: adding firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,15 @@ import 'package:responsive_builder/responsive_builder.dart';
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() => runApp(
-    ResponsiveApp(builder: (context) => ModularApp(module: AppModule(), child: AppWidget())),
-);
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+    );
+    runApp(
+        ResponsiveApp(builder: (context) => ModularApp(module: AppModule(), child: AppWidget())),
+    );
+}
