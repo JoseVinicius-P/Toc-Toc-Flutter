4f12a065bdeccf00603d06cc99f56c0b20aa1dd0
JoseVinicius-P <josevpf123@gmail.com>
2023-07-05 21:59:50 -0300

    feat: deleting unnecessary module

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:toctoc/app/modules/complete_registration/completeRegistration_module.dart';
-import 'package:toctoc/app/modules/create_account/createAccount_module.dart';
 import 'package:toctoc/app/modules/home/home_module.dart';
 import 'package:toctoc/app/modules/login/login_module.dart';
 import 'package:toctoc/app/modules/splash_screen/splash_screen_module.dart';
@@ -16,7 +15,6 @@ class AppModule extends Module {
     ModuleRoute('/login', module: LoginModule()),
     ModuleRoute('/home', module: HomeModule()),
     ModuleRoute('/complete_registration', module: CompleteRegistrationModule()),
-    ModuleRoute('/create_account', module: CreateAccountModule()),
   ];
 
 }
