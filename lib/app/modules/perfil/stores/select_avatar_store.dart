07701e473804c47bcc602517a6822938528e4402
JoseVinicius-P <josevpf123@gmail.com>
2023-07-10 23:32:54 -0300

    feat: Adding Image Cropper plugin

--- lib/app/modules/perfil/stores/select_avatar_store.dart
+++ lib/app/modules/perfil/stores/select_avatar_store.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_triple/flutter_triple.dart';
 import 'package:toctoc/app/modules/perfil/services/image_picker_service.dart';
+import 'package:image_cropper/image_cropper.dart';
 
 class SelectAvatarStore extends Store<String> {
 
@@ -8,7 +10,23 @@ class SelectAvatarStore extends Store<String> {
   SelectAvatarStore(this.imagePickerService) : super("");
 
   void pickImage() async{
-    update(await imagePickerService.pickImage());
+    String path = await imagePickerService.pickImage();
+    CroppedFile? croppedFile = await ImageCropper().cropImage(
+      sourcePath: path,
+      aspectRatioPresets: [
+        CropAspectRatioPreset.square,
+      ],
+      uiSettings: [
+        AndroidUiSettings(
+            toolbarTitle: 'Cropper',
+            toolbarColor: Colors.transparent,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+      ],
+    );
+
+    update(croppedFile?.path ?? "");
   }
 
 }
