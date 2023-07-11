1e4fe2db76e80c958ff50827301129ce536ac1b4
JoseVinicius-P <josevpf123@gmail.com>
2023-07-10 23:58:37 -0300

    feat: Improving Image Cropper

--- lib/app/modules/perfil/stores/select_avatar_store.dart
+++ lib/app/modules/perfil/stores/select_avatar_store.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_triple/flutter_triple.dart';
 import 'package:toctoc/app/modules/perfil/services/image_picker_service.dart';
 import 'package:image_cropper/image_cropper.dart';
+import 'package:toctoc/app/shared/my_colors.dart';
 
 class SelectAvatarStore extends Store<String> {
 
@@ -13,16 +14,25 @@ class SelectAvatarStore extends Store<String> {
     String path = await imagePickerService.pickImage();
     CroppedFile? croppedFile = await ImageCropper().cropImage(
       sourcePath: path,
+      cropStyle: CropStyle.circle,
+      compressFormat: ImageCompressFormat.jpg,
+      compressQuality: 90,
       aspectRatioPresets: [
         CropAspectRatioPreset.square,
       ],
       uiSettings: [
         AndroidUiSettings(
-            toolbarTitle: 'Cropper',
-            toolbarColor: Colors.transparent,
-            toolbarWidgetColor: Colors.white,
-            initAspectRatio: CropAspectRatioPreset.original,
-            lockAspectRatio: false),
+          backgroundColor: Colors.white,
+          toolbarTitle: 'Cortar',
+          toolbarColor: Colors.white,
+          toolbarWidgetColor: Colors.black,
+          activeControlsWidgetColor: MyColors.textColor,
+          initAspectRatio: CropAspectRatioPreset.square,
+          lockAspectRatio: true,
+          dimmedLayerColor: Colors.black.withOpacity(0.1),
+          statusBarColor: Colors.blue,
+          cropFrameColor: Colors.white,
+        ),
       ],
     );
 
