2a8096af0a5980c2e236fa0c76aadb32c400d94a
JoseVinicius-P <josevpf123@gmail.com>
2023-07-13 23:05:39 -0300

    feat: Improving routing

--- lib/app/modules/splash_screen/auth_guard_service.dart
+++ lib/app/modules/splash_screen/auth_guard_service.dart
@@ -5,7 +5,7 @@ import 'package:toctoc/app/shared/services/auth_service.dart';
 
 class AuthGuardService extends RouteGuard{
 
-  AuthGuardService() : super(redirectTo: '/complete_registration/');
+  AuthGuardService() : super(redirectTo: '/perfil/');
 
   @override
   FutureOr<bool> canActivate(String path, ParallelRoute route) {
