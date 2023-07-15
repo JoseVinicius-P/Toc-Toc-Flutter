e289ee5d3e2127c59d2c960d899c8c9397afad7a
JoseVinicius-P <josevpf123@gmail.com>
2023-07-15 17:17:39 -0300

    refactor: Improving audio reproduction

--- lib/app/modules/perfil/stores/sound_store.dart
+++ lib/app/modules/perfil/stores/sound_store.dart
@@ -1,44 +1,18 @@
+import 'dart:typed_data';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_triple/flutter_triple.dart';
-import 'package:just_audio/just_audio.dart';
 
 class SoundStore extends Store<int> {
 
   SoundStore() : super(-1);
 
   void playSound(String path) async {
-    AudioPlayer player = AudioPlayer();
-    try{
-      await player.setAsset(path);
-    }on PlayerException catch (e) {
-      // iOS/macOS: maps to NSError.code
-      // Android: maps to ExoPlayerException.type
-      // Web: maps to MediaError.code
-      // Linux/Windows: maps to PlayerErrorCode.index
-      print("Error code: ${e.code}");
-      // iOS/macOS: maps to NSError.localizedDescription
-      // Android: maps to ExoPlaybackException.getMessage()
-      // Web/Linux: a generic message
-      // Windows: MediaPlayerError.message
-      print("Error message: ${e.message}");
-    } on PlayerInterruptedException catch (e) {
-      // This call was interrupted since another audio source was loaded or the
-      // player was stopped or disposed before this audio source could complete
-      // loading.
-      print("Connection aborted: ${e.message}");
-    } catch (e) {
-      // Fallback for all other errors
-      print('An error occured: $e');
-    }
-
-    player.playbackEventStream.listen((event) {}, onError: (Object e, StackTrace st) {
-      if (e is PlayerException) {
-        print('Error code: ${e.code}');
-        print('Error message: ${e.message}');
-      } else {
-        print('An error occurred: $e');
-      }
-    });
-
+    ByteData byteData = await rootBundle.load(path);
+    Uint8List audioBytes = byteData.buffer.asUint8List();
+    AudioPlayer audioPlayer = AudioPlayer();
+    int result = await audioPlayer.playBytes(audioBytes);
   }
 
 }
