4c75fb1a02f44909eb6f9a6f0fdbd1b9e2d1269d
JoseVinicius-P <josevpf123@gmail.com>
2023-08-07 22:14:57 -0300

    feat: Deploying function to send notifications

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -1,31 +1,29 @@
 const functions = require("firebase-functions");
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.sendNotificationOnCallCreation = functions.firestore
-  .document('Users/{userId}/Chamadas/{chamadaId}')
-  .onCreate(async (snapshot, context) => {
-    const chamadaData = snapshot.data();
-    const userId = context.params.userId;
+    .document("Users/{userId}/Chamadas/{chamadaId}")
+    .onCreate(async (snapshot, context) => {
+      const userId = context.params.userId;
 
-    // Obter o token de notificação do usuário
-    const userDocRef = admin.firestore().doc(`Users/${userId}`);
-    const userDoc = await userDocRef.get();
-    const userData = userDoc.data();
-    const userToken = userData.token; // Substitua 'token' pelo nome do campo que contém o token de notificação
+      const userDocRef = admin.firestore().doc("Users/"+userId);
+      const userDoc = await userDocRef.get();
+      const userData = userDoc.data();
+      const userToken = userData.token;
 
-    const message = {
-      notification: {
-        title: 'Nova Chamada',
-        body: 'Uma nova chamada foi criada.',
-      },
-      token: userToken,
-    };
+      const message = {
+        notification: {
+          title: "Nova Chamada",
+          body: "Uma nova chamada foi criada.",
+        },
+        token: userToken,
+      };
 
-    try {
-      const response = await admin.messaging().send(message);
-      console.log('Notificação enviada com sucesso:', response);
-    } catch (error) {
-      console.error('Erro ao enviar a notificação:', error);
-    }
-  });
+      try {
+        const response = await admin.messaging().send(message);
+        console.log("Notificação enviada com sucesso:", response);
+      } catch (error) {
+        console.error("Erro ao enviar a notificação:", error);
+      }
+    });
