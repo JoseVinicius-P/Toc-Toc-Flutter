f51675f87138dacc84000424451ce365f4b83d9a
JoseVinicius-P <josevpf123@gmail.com>
2023-08-08 21:39:46 -0300

    fix: fixed the bug that assigned the same token to different accounts logged in on the same device

--- lib/app/modules/perfil/perfil_controller.dart
+++ lib/app/modules/perfil/perfil_controller.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:toctoc/app/modules/perfil/your_data/services/image_picker_service.dart';
 import 'package:toctoc/app/modules/perfil/select_sound/local_sounds_service.dart';
 
@@ -37,8 +39,11 @@ class PerfilController implements Disposable{
   }
 
   void signOut() async {
-    await FirebaseAuth.instance.signOut().then((value) => Modular.to.navigate('/login/'));
-
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if(await prefs.clear()){
+      await FirebaseMessaging.instance.deleteToken();
+      await FirebaseAuth.instance.signOut().then((value) => Modular.to.navigate('/login/'));
+    }
   }
 
 }
