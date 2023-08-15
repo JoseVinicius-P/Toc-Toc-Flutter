f9c34f88d3d00669228d38d1db6f648d0856c4de
JoseVinicius-P <josevpf123@gmail.com>
2023-08-15 20:41:33 -0300

    feat: Adding guard service

--- lib/app/modules/splash_screen/splash_screen_module.dart
+++ lib/app/modules/splash_screen/splash_screen_module.dart
@@ -1,3 +1,4 @@
+import 'package:toctoc/app/modules/splash_screen/notification_guard_service.dart';
 import 'package:toctoc/app/modules/splash_screen/auth_guard_service.dart';
 import 'package:toctoc/app/modules/splash_screen/splash_screen_controller.dart';
 import 'package:toctoc/app/modules/splash_screen/splash_screen_page.dart';
@@ -7,13 +8,14 @@ import 'package:flutter_modular/flutter_modular.dart';
 class SplashScreenModule extends Module {
   @override
   final List<Bind> binds = [
+    Bind.lazySingleton((i) => NotificationGuardService()),
     Bind.lazySingleton((i) => SplashScreenStore()),
     Bind.lazySingleton((i) => SplashScreenController()),
   ];
 
   @override
   final List<ModularRoute> routes = [
-    ChildRoute('/', child: (_, args) => SplashScreenPage(), guards: [AuthGuardService()]),
+    ChildRoute('/', child: (_, args) => SplashScreenPage(), guards: [AuthGuardService()/*, NotificationGuardService()*/]),
   ];
 
 }
