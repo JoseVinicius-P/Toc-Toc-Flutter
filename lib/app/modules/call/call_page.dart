b7ad066b6757628f09c6c7c70d7fa291beb8ef00
JoseVinicius-P <josevpf123@gmail.com>
2023-08-17 23:02:02 -0300

    style: Creating layout of the call page

--- lib/app/modules/call/call_page.dart
+++ lib/app/modules/call/call_page.dart
@@ -1,6 +1,9 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 import 'package:toctoc/app/modules/call/call_store.dart';
 import 'package:flutter/material.dart';
+import 'package:toctoc/app/shared/my_colors.dart';
 
 class CallPage extends StatefulWidget {
   final String title;
@@ -13,13 +16,69 @@ class CallPageState extends State<CallPage> {
 
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[],
-      ),
+        body: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              color: Colors.white,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 30.0, top: 10.0),
+              child: SafeArea(
+                child: Stack(
+                  children: [
+                    Column(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 35,),
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: 40.sw.roundToDouble(),
+                              height: 40.sw.roundToDouble(),
+                              child:  CircleAvatar(
+                                backgroundImage: const AssetImage('assets/images/perfil.png'),
+                                /*foregroundImage: NetworkImage(
+                                    user.profilePictureUrl
+                                ),*/
+                                radius: 40.sw.roundToDouble(),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10,),
+                        AutoSizeText(
+                          'Rodrigo',
+                          style: theme.textTheme.titleSmall!.copyWith(fontSize: 25),
+                          maxFontSize: 6.sw.roundToDouble(),
+                          minFontSize: 3.sw.roundToDouble(),
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 5,),
+                        AutoSizeText(
+                          'Você está sendo visitado!',
+                          style: theme.textTheme.labelMedium!.copyWith(fontSize: 18, color: MyColors.textColor.withOpacity(0.3)),
+                          maxFontSize: 6.sw.roundToDouble(),
+                          minFontSize: 3.sw.roundToDouble(),
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ],
+                ) ,
+              ),
+            ),
+          ],
+        )
     );
   }
 }
