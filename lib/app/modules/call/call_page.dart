2514a853db06400842b22c00e10f9d130e89800c
JoseVinicius-P <josevpf123@gmail.com>
2023-08-22 21:51:37 -0300

    style: Adding button exit

--- lib/app/modules/call/call_page.dart
+++ lib/app/modules/call/call_page.dart
@@ -67,51 +67,47 @@ class CallPageState extends State<CallPage> {
                 return Padding(
                   padding: const EdgeInsets.only(left: 30.0, right: 30.0, top: 10.0),
                   child: SafeArea(
-                    child: Stack(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Column(
+                        const SizedBox(height: 35,),
+                        Row(
                           mainAxisSize: MainAxisSize.max,
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            const SizedBox(height: 35,),
-                            Row(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                  width: 40.sw.roundToDouble(),
-                                  height: 40.sw.roundToDouble(),
-                                  child:  CircleAvatar(
-                                    backgroundImage: const AssetImage('assets/images/perfil.png'),
-                                    foregroundImage: CachedNetworkImageProvider(
-                                        data['profilePictureUrl']
-                                    ),
-                                    radius: 40.sw.roundToDouble(),
-                                  ),
+                            SizedBox(
+                              width: 40.sw.roundToDouble(),
+                              height: 40.sw.roundToDouble(),
+                              child:  CircleAvatar(
+                                backgroundImage: const AssetImage('assets/images/perfil.png'),
+                                foregroundImage: CachedNetworkImageProvider(
+                                    data['profilePictureUrl']
                                 ),
-                              ],
-                            ),
-                            const SizedBox(height: 10,),
-                            AutoSizeText(
-                              data['name'],
-                              style: theme.textTheme.titleSmall!.copyWith(fontSize: 25),
-                              maxFontSize: 6.sw.roundToDouble(),
-                              minFontSize: 3.sw.roundToDouble(),
-                              maxLines: 3,
-                              overflow: TextOverflow.ellipsis,
-                              textAlign: TextAlign.center,
-                            ),
-                            const SizedBox(height: 5,),
-                            AutoSizeText(
-                              'Você está recebendo uma visita!',
-                              style: theme.textTheme.labelMedium!.copyWith(fontSize: 18, color: MyColors.textColor.withOpacity(0.3)),
-                              maxFontSize: 6.sw.roundToDouble(),
-                              minFontSize: 3.sw.roundToDouble(),
-                              maxLines: 3,
-                              overflow: TextOverflow.ellipsis,
-                              textAlign: TextAlign.center,
+                                radius: 40.sw.roundToDouble(),
+                              ),
                             ),
                           ],
+                        ),
+                        const SizedBox(height: 10,),
+                        AutoSizeText(
+                          data['name'],
+                          style: theme.textTheme.titleSmall!.copyWith(fontSize: 25),
+                          maxFontSize: 6.sw.roundToDouble(),
+                          minFontSize: 3.sw.roundToDouble(),
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 5,),
+                        AutoSizeText(
+                          widget.receivingCall ? 'Você está recebendo uma visita!' : 'Você está fazendo uma visita!',
+                          style: theme.textTheme.labelMedium!.copyWith(fontSize: 18, color: MyColors.textColor.withOpacity(0.3)),
+                          maxFontSize: 6.sw.roundToDouble(),
+                          minFontSize: 3.sw.roundToDouble(),
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
                         ),
                       ],
                     ) ,
@@ -119,104 +115,148 @@ class CallPageState extends State<CallPage> {
                 );
               }
             ),
-            TripleBuilder(
-              store: store,
-              builder: (context, triple) {
-                var data = triple.state as Map<String, dynamic>;
-                return Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: MyColors.errorRed, // Altera a cor de fundo
-                              shape: const CircleBorder(),
-                              disabledBackgroundColor: MyColors.errorRed,
-                            ),
-                            onPressed: () {
-                              stopTimer();
-                              store.sendReply("Não está em casa!", data['callId']);
-                            },
-                            child: const Padding(
-                              padding: EdgeInsets.all(13.0),
-                              child: Icon(Icons.close, color: Colors.white,),
-                            ),
+            Visibility(
+              visible: !widget.receivingCall,
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: MyColors.blue, // Altera a cor de fundo
+                            shape: const CircleBorder(),
+                            disabledBackgroundColor: MyColors.blue,
                           ),
-                          AutoSizeText(
-                            'Não estou\nem casa',
-                            style: theme.textTheme.titleSmall!.copyWith(fontSize: 15),
-                            maxFontSize: 6.sw.roundToDouble(),
-                            minFontSize: 3.sw.roundToDouble(),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            textAlign: TextAlign.center,
+                          onPressed: () {
+                            stopTimer();
+                            Modular.to.pop();
+                          },
+                          child: const Padding(
+                            padding: EdgeInsets.all(18.0),
+                            child: Icon(Icons.close_rounded, color: Colors.white, size: 30,),
                           ),
-                        ],
-                      ),
-                      Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: MyColors.blue, // Altera a cor de fundo
-                              shape: const CircleBorder(),
-                              disabledBackgroundColor: MyColors.blue,
+                        ),
+                        AutoSizeText(
+                          'Fechar',
+                          style: theme.textTheme.titleSmall!.copyWith(fontSize: 18),
+                          maxFontSize: 6.sw.roundToDouble(),
+                          minFontSize: 3.sw.roundToDouble(),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 15,),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Visibility(
+              visible: widget.receivingCall,
+              child: TripleBuilder(
+                store: store,
+                builder: (context, triple) {
+                  var data = triple.state as Map<String, dynamic>;
+                  return Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: MyColors.errorRed, // Altera a cor de fundo
+                                shape: const CircleBorder(),
+                                disabledBackgroundColor: MyColors.errorRed,
+                              ),
+                              onPressed: () {
+                                stopTimer();
+                                store.sendReply("Não está em casa!", data['callId']);
+                              },
+                              child: const Padding(
+                                padding: EdgeInsets.all(13.0),
+                                child: Icon(Icons.close, color: Colors.white,),
+                              ),
                             ),
-                            onPressed: () {
-                              stopTimer();
-                              store.sendReply("Está vindo atender!", data['callId']);
-                            },
-                            child: const Padding(
-                              padding: EdgeInsets.all(18.0),
-                              child: Icon(Icons.meeting_room_outlined, color: Colors.white, size: 30,),
+                            AutoSizeText(
+                              'Não estou\nem casa',
+                              style: theme.textTheme.titleSmall!.copyWith(fontSize: 15),
+                              maxFontSize: 6.sw.roundToDouble(),
+                              minFontSize: 3.sw.roundToDouble(),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.center,
                             ),
-                          ),
-                          AutoSizeText(
-                            'Estou indo!\n ',
-                            style: theme.textTheme.titleSmall!.copyWith(fontSize: 18),
-                            maxFontSize: 6.sw.roundToDouble(),
-                            minFontSize: 3.sw.roundToDouble(),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            textAlign: TextAlign.center,
-                          ),
-                          SizedBox(height: 15,),
-                        ],
-                      ),
-                      Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: MyColors.lightGray, // Altera a cor de fundo
-                              shape: const CircleBorder(),
-                              disabledBackgroundColor: MyColors.lightGray,
+                          ],
+                        ),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: MyColors.blue, // Altera a cor de fundo
+                                shape: const CircleBorder(),
+                                disabledBackgroundColor: MyColors.blue,
+                              ),
+                              onPressed: () {
+                                stopTimer();
+                                store.sendReply("Está vindo atender!", data['callId']);
+                              },
+                              child: const Padding(
+                                padding: EdgeInsets.all(18.0),
+                                child: Icon(Icons.meeting_room_outlined, color: Colors.white, size: 30,),
+                              ),
                             ),
-                            onPressed: (){},
-                            child: const Padding(
-                              padding: EdgeInsets.all(13.0),
-                              child: Icon(Icons.door_back_door_outlined, color: MyColors.textColor,),
+                            AutoSizeText(
+                              'Estou indo!\n ',
+                              style: theme.textTheme.titleSmall!.copyWith(fontSize: 18),
+                              maxFontSize: 6.sw.roundToDouble(),
+                              minFontSize: 3.sw.roundToDouble(),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.center,
                             ),
-                          ),
-                          AutoSizeText(
-                            'Ignorar\n ',
-                            style: theme.textTheme.titleSmall!.copyWith(fontSize: 15),
-                            maxFontSize: 6.sw.roundToDouble(),
-                            minFontSize: 3.sw.roundToDouble(),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            textAlign: TextAlign.center,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              }
+                            SizedBox(height: 15,),
+                          ],
+                        ),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: MyColors.lightGray, // Altera a cor de fundo
+                                shape: const CircleBorder(),
+                                disabledBackgroundColor: MyColors.lightGray,
+                              ),
+                              onPressed: (){},
+                              child: const Padding(
+                                padding: EdgeInsets.all(13.0),
+                                child: Icon(Icons.door_back_door_outlined, color: MyColors.textColor,),
+                              ),
+                            ),
+                            AutoSizeText(
+                              'Ignorar\n ',
+                              style: theme.textTheme.titleSmall!.copyWith(fontSize: 15),
+                              maxFontSize: 6.sw.roundToDouble(),
+                              minFontSize: 3.sw.roundToDouble(),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              ),
             )
           ],
         )
